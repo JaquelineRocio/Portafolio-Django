41fa1ff46c35f5a690da5e3390a24e67ce14a001
JaquelineRocio <jaquelineramosvargas@gmail.com>
2022-12-11 17:03:32 -0500

    Implementando el login

---
+++ portafolio/users/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import redirect
+from django.http import HttpResponse
+from .forms import NewUserForm
+from django.views.generic import CreateView
+
+# Create your views here.
+class RegisterView(CreateView):
+  template_name = "registration/register.html"
+  form_class = NewUserForm
+
+  def form_valid(self, form):
+      form.save()
+      return redirect('login')
